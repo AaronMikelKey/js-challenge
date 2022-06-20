4b086513e26d17c7991aace57018b5b71de66590
Aaron Key <AaronMikelKey@gmail.com>
2022-06-20 13:51:02 -0400

    Shortened and reformatted code
    Added uppercase character array since it was shorter than converting values. Added a function to validate a character type selection. Changed if/else statement to separate if statements.

--- Develop/script.js
+++ Develop/script.js
@@ -1,71 +1,64 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// vars for password type
+// vars for password characters
 let passwordLength, uppercaseIncluded, lowercaseIncluded, numericIncluded, specialIncluded;
 // Possible character arrays
-const letters = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
-const numbers = ['1','2','3','4','5','6','7','8','9','0'];
-const specials = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '~', '`', '|', ';', ':', '<', ',','>','.','?','/'];
+const lowercaseLetters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const uppercaseLetters = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+const specials = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '~', '`', '|', ';', ':', '<', ',', '>', '.', '?', '/'];
 
 // Password Length validation
 const checkPasswordLength = (passwordLength) => {
 	return passwordLength > 8 && passwordLength < 128 ? true : new Error('Password length must be 8-128 characters.')
 }
 
+// One type of character selected validation
+const checkCharactersSelected = () => {
+	//undefined so falsy
+	let bool;
+	if (uppercaseIncluded || lowercaseIncluded || numericIncluded || specialIncluded) {
+		bool = true;
+	}
+	return bool;
+}
+
 // Write password to the #password input
 function writePassword() {
 	passwordLength = Number(window.prompt('Enter desired password length (8-128): '));
-	if (passwordLength < 8 || passwordLength > 128 ) {
+	if (passwordLength < 8 || passwordLength > 128) {
 		alert('Password must be 8-128 characters.');
-		writePassword();
+		writePassword()
 	} else {
-	uppercaseIncluded = window.confirm('Include uppercase characters?');
-	lowercaseIncluded = window.confirm('Include lowercase characters?');
-	
-	numericIncluded = window.confirm('Include numeric characters?');
-	specialIncluded = window.confirm('Include special characters?');
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-	console.log(passwordLength)
+		uppercaseIncluded = window.confirm('Include uppercase characters?');
+		lowercaseIncluded = window.confirm('Include lowercase characters?');
+		numericIncluded = window.confirm('Include numeric characters?');
+		specialIncluded = window.confirm('Include special characters?');
+		checkCharactersSelected() ? null : (alert('One type of character must be included.'), writePassword())
+		var password = generatePassword();
+		var passwordText = document.querySelector("#password");
+		passwordText.value = password;
 	}
 }
 
-// Generate Password from prompts
-
+// Generate Password from prompt selections
 const generatePassword = () => {
 	let passwordArr = [];
 	let charactersArr = [];
-	
-	// Uppercase and lowercase included
-	if (uppercaseIncluded && lowercaseIncluded) {
-		letters.forEach(letter => letters.push(letter.toUpperCase()))
+	// Uppercase included, add letters to character array 
+	if (uppercaseIncluded) { charactersArr = charactersArr.concat(uppercaseLetters) };
+	// Lowercase included, add lowercase letters to character array
+	if (lowercaseIncluded) { charactersArr = charactersArr.concat(lowercaseLetters) };
+	// Special included, add specials to character array
+	if (specialIncluded) (charactersArr = charactersArr.concat(specials));
+	// Numbers included, add numbers to character array
+	if (numericIncluded) (charactersArr = charactersArr.concat(numbers));
+	// Randomly select character from character array and add to password array
+	for (let i = 0; i < passwordLength; i++) {
+		let num = Math.floor(Math.random() * charactersArr.length)
+		passwordArr.push(charactersArr[num])
 	}
 
-	// All included
-	if (uppercaseIncluded && lowercaseIncluded && numericIncluded && specialIncluded) {
-		charactersArr = letters.concat(numbers, specials)
-		// All but special included
-	} else if (uppercaseIncluded && lowercaseIncluded && numericIncluded && !specialIncluded) {
-		charactersArr = letters.concat(numbers)
-		// All but numbers included
-	} else if (uppercaseIncluded && lowercaseIncluded && !numericIncluded && specialIncluded) {
-		charactersArr = letters.concat(specials)
-		// All letters
-	} else if ( uppercaseIncluded && lowercaseIncluded && !numericIncluded && !specialIncluded) {
-		charactersArr = letters
-	}
-
-	console.log(charactersArr)
-	
-		for (let i=0; i<passwordLength; i++) {
-			let num = Math.floor(Math.random() * charactersArr.length)
-			passwordArr.push(charactersArr[num])
-			console.log(passwordArr)
-		}
-	
 	return (passwordArr.join(''))
 }
 
